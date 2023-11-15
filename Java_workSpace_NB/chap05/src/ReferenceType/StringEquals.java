281b9f09d2d22964a8d05da1c0cfcafd3aaacd30
jinseong-1 <dwlstjdb9743@gmail.com>
2023-11-15 21:30:03 +0900

    11/15 home review

---
+++ Java_workSpace_NB/chap05/src/ReferenceType/StringEquals.java
@@ -0,0 +1,35 @@
+package ReferenceType;
+
+public class StringEquals {
+
+	public static void main(String[] args) {
+		String strVar1 = "신민철";
+		String strVar2 = "신민철";
+		
+		if(strVar1 == strVar2) {
+			System.out.println("strVar1과 strVar2는 참조가 같음");	//이 값이 출력
+		}else {
+			System.out.println("strVar1과 strVar2는 참조가 다름");
+		}
+		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("strVar1과 strVar2는 문자열이 같음");
+		}
+		
+		String strVar3 = new String("신민철");
+		String strVar4 = new String("신민철");
+		
+		if(strVar3 == strVar4) {
+			System.out.println("strVar3과 strVar4는 참조가 같음");
+		}else {
+			System.out.println("strVar3과 strVar4는 참조가 다름");	// 이 값이 출력
+		}
+		// new라고 새롭게 heap영역에 배정하였기에 단순비교하면 위치값만 비교되어 else값이 출력
+		
+		if(strVar3.equals(strVar4)) {
+			System.out.println("strVar3과 strVar4는 문자열이 같음");
+		}
+		
+	}
+
+}
