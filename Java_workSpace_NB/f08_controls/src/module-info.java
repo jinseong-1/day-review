83d334fa1f8e531e394d180f027b57f5547608ca
jinseong-1 <dwlstjdb9743@gmail.com>
2023-12-29 21:22:20 +0900

    chatserver, client

--- Java_workSpace_NB/f08_controls/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module f08_controls {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.web;
-	requires javafx.graphics;
-	
-	opens c1_button_basic to javafx.graphics, javafx.fxml;
-}
